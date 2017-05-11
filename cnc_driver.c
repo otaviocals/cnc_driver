0c84d06f5ad798e14c836f8bcd14b030a33a5fc9
otaviocals <otaviolcals@gmail.com>
2017-05-11 10:57:54 -0300

    added stepper 2 buttons

--- cnc_driver.c
+++ cnc_driver.c
@@ -2,9 +2,14 @@ const int ledPin1 =  8;
 const int ledPin2 =  9;
 const int ledPin3 =  10;
 const int ledPin4 =  11;
-const int forw_but = 5;
-const int back_but = 6;
-const int led_ind = 13;
+
+const int forw_but_2 = 3;
+const int back_but_2 = 4;
+const int led_ind_2 = 12;
+
+const int forw_but_1 = 5;
+const int back_but_1 = 6;
+const int led_ind_1 = 13;
 
 const int step_delay = 40;
 
@@ -23,7 +28,7 @@ void setup() {
   pinMode(led_ind, OUTPUT);
 }
 
-void forward()
+void forward_1()
 {
   //A
   digitalWrite(ledPin1, HIGH);
@@ -76,7 +81,7 @@ void forward()
 
 }
 
-void backward()
+void backward_1()
 {
   //H
   digitalWrite(ledPin1, HIGH);
@@ -130,22 +135,41 @@ void backward()
 
 void loop()
 {
-  //forward();
-  forw_state = digitalRead(forw_but);
-  back_state = digitalRead(back_but);
+  //Stepper 1
+  forw_state_1 = digitalRead(forw_but_1);
+  back_state_1 = digitalRead(back_but_1);
 
-  if (forw_state == HIGH && back_state == LOW)
+  if (forw_state_1 == HIGH && back_state_1 == LOW)
   {
-    digitalWrite(led_ind, HIGH);
+    digitalWrite(led_ind_1, HIGH);
     forward();
   }
-  else if (forw_state == LOW && back_state == HIGH)
+  else if (forw_state_1 == LOW && back_state_1 == HIGH)
   {
-    digitalWrite(led_ind, HIGH);
+    digitalWrite(led_ind_1, HIGH);
     backward();
   }
   else
   {
-    digitalWrite(led_ind, LOW);
+    digitalWrite(led_ind_1, LOW);
+  }
+
+  //Stepper 2
+  forw_state_2 = digitalRead(forw_but_2);
+  back_state_2 = digitalRead(back_but_2);
+
+  if (forw_state_2 == HIGH && backstate_2 == LOW)
+  {
+      digitalWrite(led_ind_2, HIGH);
+      forward_1();
+  }
+  else if (forw_state_2 == LOW && back_state_2 == HIGH)
+  {
+      digitalWrite(led_ind_2, HIGH);
+      backward_1();
+  }
+  else
+  {
+      digitalWrite(led_ind_2, LOW);
   }
 }
